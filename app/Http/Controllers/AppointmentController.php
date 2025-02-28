41168870c1eb63e9ede8e5094b8c834bf183fb2e
FeketeAbel <feketeabelard@gmail.com>
2025-02-28 09:03:15 +0100

    index,store

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Barber;
+use Exception;
 use Illuminate\Http\Request;
 use App\Models\Appointment;
+use Illuminate\Validation\ValidationException;
 
 class AppointmentController extends Controller
 {
@@ -13,10 +16,39 @@ class AppointmentController extends Controller
     }
 
     public function store(Request $request) {
-        
+        try {
+            $request->validate([
+                "name" => "required|string|max:255",
+                "barber_id" => "required|exists:barbers,id",
+                "appointment" => "required|date",
+            ], [
+                "string"=> "A(z) :attribute mezőnek szövegnek kell lennie!",
+                "name.required" => "A(z) :attribute mező megadása kötelező!",
+                "barber_id.required" => "A(z) :attribute mező megadása kötelező!",
+                "appointment.required" => "A(z) :attribute mező megadása kötelező!",
+                "exists"=> "A(z) :attribute mezőnek egy létező barberhez kell tartoznia!",
+                "max" => "A(z) :attribute mező túl hosszú! Max: :max",
+                "date"=> "A(z) :attribute mezőnek dátum típusúnak kell lennie!",
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json(["success" => false, "uzenet" => $e->errors()], 400, ["Access-Control-Allow-Origin" => "*"], JSON_UNESCAPED_UNICODE);
+        }
+
+        $appointment = Appointment::create($request->all());
+        return response()->json(["success" => true, "uzenet" => $appointment->name." sikeresen rögzítésre került"], 200, ["Access-Control-Allow-Origin" => "*"], JSON_UNESCAPED_UNICODE);
     }
 
     public function destroy(Request $request) {
+        try {
+            $appointment = Appointment::findOrFail($request->id);
+        } catch (Exception $e) {
+            return response()->json(["success" => false, "uzenet" => "Nem létező időpont!"], 400, options: JSON_UNESCAPED_UNICODE);
+        }
 
+        if ($appointment->delete()) {
+            return response()->json(["success" => true, "uzenet" => "Sikeres törlés!"], 200, options: JSON_UNESCAPED_UNICODE);
+        } else {
+            return response()->json(["success" => false, "uzenet" => "Sikertelen törlés!"], 400, options: JSON_UNESCAPED_UNICODE);
+        }
     }
 }
--- app/Http/Controllers/BarberController.php
+++ app/Http/Controllers/BarberController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Barber;
+use Illuminate\Validation\ValidationException;
 
 class BarberController extends Controller
 {
@@ -13,7 +14,22 @@ class BarberController extends Controller
     }
 
     public function store(Request $request) {
+        try {
+            $request->validate([
+                "barber_name" => "required|string|max:255",
+            ], [
+                "required"=> "A(z) :attribute mezőt kötelező kitölteni!",
+                "string"=> "A(z) :attribute mezőnek szöveg típusúnak kell lennie!",
+                "max"=> "A(z) :attribute mező túl hosszú! Max: :max",
+            ], [
+                "barber_name" => "Barber neve",
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json(["success" => false, "uzenet" => $e->errors()], 400, ["Access-Control-Allow-Origin" => "*"], JSON_UNESCAPED_UNICODE);
+        }
 
+        $barber = Barber::create($request->all());
+        return response()->json(["success" => true, "uzenet" => $barber->barber_name." sikeresen rögzítésre került"], 200, ["Access-Control-Allow-Origin" => "*"], JSON_UNESCAPED_UNICODE);
     }
 
     public function destroy(Request $request) {
